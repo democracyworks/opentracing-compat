57ff05749c42da381adcf89380548fce5b2739bb
Gordon Stratton <gordon@democracy.works>
2019-07-30 17:56:53 -0700

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/opentracing-compat "0.1.0"
+(defproject democracyworks/opentracing-compat "0.1.1-SNAPSHOT"
   :description "A compatibility library bridging clj-newrelic with the OpenTracing API."
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
